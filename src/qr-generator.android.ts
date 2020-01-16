06738089ba8810986d891a87406bdfab76ff19b9
erodriguezh <taishogun@gmail.com>
2020-01-16 22:06:35 +0100

    config gradle and add base call from plugin example

--- src/qr-generator.android.ts
+++ src/qr-generator.android.ts
@@ -1,5 +1,32 @@
-import { Common } from './qr-generator.common';
+import { QROptions } from './qr-generator.common';
+import { Color } from 'tns-core-modules/color';
 
-export class QrGenerator extends Common {
+export class QrGenerator {
+  render(value: string, options?: QROptions): UIImage {
+    if (typeof options === 'undefined') options = {};
 
+    const qr = new QRCodeGenerator({ string: value });
+
+    if (options.size) {
+      qr.size = CGSizeMake(options.size.width, options.size.height);
+    }
+
+    if (options.color) {
+      const uiColor = new Color(options.color);
+      qr.color = CIColor.colorWithRedGreenBlue(uiColor.r, uiColor.g, uiColor.b);
+    }
+
+    if (options.backgroundColor) {
+      const uiBackgroundColor = new Color(options.backgroundColor);
+      qr.backgroundColor = CIColor.colorWithRedGreenBlue(uiBackgroundColor.r, uiBackgroundColor.g, uiBackgroundColor.b);
+    }
+
+    return qr.getImage();
+/* 
+    Bitmap myBitmap = QRCode.from(this.qrCodeValue).withSize(this.size, this.size)
+    .withColor(this.onColor, this.offColor).bitmap();
+
+    mImageView.setImageBitmap(myBitmap);
+     */
+  }
 }
