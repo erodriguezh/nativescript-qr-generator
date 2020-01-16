1133c86a42dcd3299d41966248d386293c29c9bb
erodriguezh <taishogun@gmail.com>
2020-01-16 21:17:00 +0100

    finish demo and ios integration

--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
     onImageLoaded(args) {
         const image = args.object.ios as UIImageView;
-        const result = new QrGenerator().render('Hello World', { color: '#FF0000' });
+        const result = new QrGenerator().render('Hello World', { color: '#FF0000', backgroundColor: '#FFFF00', size: { width: 300, height: 300 } });
         image.image = result;
     }
 }
--- src/qr-generator.ios.ts
+++ src/qr-generator.ios.ts
@@ -1,11 +1,14 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./typings/qr-code-generator.ios.d.ts" />
 
-import { Common, QROptions } from './qr-generator.common';
+import { QROptions } from './qr-generator.common';
+import { Color } from 'tns-core-modules/color';
 
-export class QrGenerator extends Common {
+export class QrGenerator {
 
   render(value: string, options?: QROptions): UIImage {
+    if (typeof options === 'undefined') options = {};
+
     const qr = new QRCodeGenerator({ string: value });
 
     if (options.size) {
@@ -13,26 +16,16 @@ export class QrGenerator extends Common {
     }
 
     if (options.color) {
-      qr.color = CIColor.colorWithString(options.color);
+      const uiColor = new Color(options.color);
+      qr.color = CIColor.colorWithRedGreenBlue(uiColor.r, uiColor.g, uiColor.b);
     }
 
     if (options.backgroundColor) {
-      qr.backgroundColor = CIColor.colorWithString(options.backgroundColor);
+      const uiBackgroundColor = new Color(options.backgroundColor);
+      qr.backgroundColor = CIColor.colorWithRedGreenBlue(uiBackgroundColor.r, uiBackgroundColor.g, uiBackgroundColor.b);
     }
 
     return qr.getImage();
-/*       qr.size = CGSizeMake(width, height);
-    qr.color = CIColor.colorWithRGBA(color);
-    qr.backgroundColor = CIColor.colorWithRGBA(color); */
-    // return <NativeQRCode bgColor={backgroundColor} fgColor={foregroundColor} {...props} style={{ width: props.size, height: props.size, ...props.style }} />
-/*
-    QRCodeGenerator *qr = [[QRCodeGenerator alloc] initWithString:@"Hello World"];
-    qr.size = CGSizeMake(400.0f, 400.0f); // 400x400 size
-    qr.color = [CIColor colorWithRGBA:@"#FFFFFF"]; // white QR Code color
-    qr.backgroundColor = [CIColor colorWithRGBA:@"#000000"]; // black background color
-
-    imageView.image = [qr getImage];
-*/
   }
 
 
