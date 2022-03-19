737ad0a1e1d0be761b3a109512ef42427d6cd26d
kefah@Cocarz <kefah.bader@gmail.com>
2022-03-19 16:07:49 +0100

    create a TS demo project

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,38 +1,36 @@
-import { Observable } from '@nativescript/core'
+import { Observable, ImageSource, Image } from '@nativescript/core';
+import { QrGenerator } from 'nativescript-qr-generator';
 
 export class HelloWorldModel extends Observable {
   private _counter: number
   private _message: string
 
   constructor() {
-    super()
-
-    // Initialize default values.
-    this._counter = 42
-    this.updateMessage()
+    super();
   }
 
-  get message(): string {
-    return this._message
-  }
-
-  set message(value: string) {
-    if (this._message !== value) {
-      this._message = value
-      this.notifyPropertyChange('message', value)
+  onImageLoadedBasic(args) {
+        const image = args.object as Image;
+        const result = new QrGenerator().generate(`Hello World`, {
+            logo: {
+                path: "~/assets/fb.png",
+                ratio: {
+                    w: 50, h: 50
+                }
+            }
+        });
+        image.imageSource = new ImageSource(result);
     }
-  }
 
-  onTap() {
-    this._counter--
-    this.updateMessage()
-  }
-
-  private updateMessage() {
-    if (this._counter <= 0) {
-      this.message = 'Hoorraaay! You unlocked the NativeScript clicker achievement!'
-    } else {
-      this.message = `${this._counter} taps left`
+    onImageLoadedColors(args) {
+        const image = args.object as Image;
+        const result = new QrGenerator().generate('Hello World with colors', { color: '#4183d7', backgroundColor: '#67809f' });
+        image.imageSource = new ImageSource(result);
     }
-  }
+
+    onImageLoadedColorsSize(args) {
+        const image = args.object as Image;
+        const result = new QrGenerator().generate('Hello World with colors and custom size', { size: { width: 100, height: 100 }, color: '#fbd90e', backgroundColor: '#0d0c0c' });
+        image.imageSource = new ImageSource(result);
+    }
 }
